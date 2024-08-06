e60388f5a304ef10e5f5c7d4f782876eb76fb5fc
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-06 23:26:53 +0330

    change repo and handle error

--- src/repositrory/user/user.repositroy.ts
+++ src/repositrory/user/user.repositroy.ts
@@ -1,6 +1,37 @@
-import { Model } from "mongoose";
+import { Document, Model } from "mongoose";
 import { IUser } from "../../db/user/user.model";
 import { Email, Name, Password, Username } from "../../types/user.types";
+ 
+export interface createUser {
+    username: Username;
+    password: Password;
+    email: Email;
+}
+
+export interface updateUser {
+    firstName: Name;
+    lastName: Name;
+    username: Username;
+    password: Password;
+    email: Email;
+    private: boolean;
+    imageUrl: string;
+    bio?: string;
+}
+
+export interface loginUser{
+    username: Username;
+    password: Password;
+    email: Email;
+}
+
+type MongoDoc<T> = (Document<unknown, {}, T> & T & Required<{
+    _id: unknown;
+}>);
+
+const handleDBError = () => {
+    throw new Error("خطای شبکه رخ داده است.")
+}
  
 export interface createUser {
     username: Username;
@@ -33,63 +64,44 @@ export class UserRepository {
       this.model = model;
     }
     
-    async createUser(userData: createUser): Promise<IUser> {
+    async createUser(userData: createUser): Promise<Boolean> {
         const user = new this.model(userData);
-        await user.save();
+        await user.save().catch((err) => handleDBError());
 
-        if(!user) {
-            throw new Error('user not created')
-        }
+        return true
+    }
+
+    async getUserByUsername(username: Username): Promise<MongoDoc<IUser> | null> {
+        const user = await this.model.findOne({username}, { _id: 0 , password : 0 })
+        .exec().catch((err) => handleDBError());
 
         return user
     }
 
-    async getUserByUsername(username: Username): Promise<IUser> {
-        const user = await this.model.findOne({username}, { _id: 0 , password : 0 }).exec();
-
-        if(!user) {
-            throw new Error('user not created')
-        }
-
-        return user
-    }
-
-    async getUserPasswordByUsername(username: Username): Promise<loginUser> {
-        const user = await this.model.findOne({username}, { _id: 0 , password : 1 , username: 1, email : 1}).exec();
-
-        if(!user) {
-            throw new Error('user not created')
-        }
+    async getUserPasswordByUsername(username: Username): Promise<MongoDoc<IUser> | null> {
+        const user = await this.model.findOne({username}, { _id: 0 , password : 1 , username: 1, email : 1})
+        .exec().catch((err) => handleDBError());;
 
         return user
     }
     
-    async getUserPasswordByEmail(email: Email): Promise<loginUser> {
-        const user = await this.model.findOne({email}, { _id: 0 , password : 1 , username: 1, email : 1 }).exec();
-
-        if(!user) {
-            throw new Error('user not created')
-        }
+    async getUserPasswordByEmail(email: Email): Promise<MongoDoc<IUser> | null> {
+        const user = await this.model.findOne({email}, { _id: 0 , password : 1 , username: 1, email : 1 })
+        .exec().catch((err) => handleDBError());;
 
         return user
     }
 
-    async updateUser(username: string, updateData: updateUser): Promise<IUser> {
-        const user = await this.model.findOneAndUpdate({username}, updateData).exec();
-
-        if(!user) {
-            throw new Error('user not created')
-        }
+    async updateUser(username: string, updateData: updateUser): Promise<MongoDoc<IUser> | null> {
+        const user = await this.model.findOneAndUpdate({username}, updateData)
+        .exec().catch((err) => handleDBError());;
 
         return user
     }
 
-    async UpdatePassword(username:Username, password: Password): Promise<IUser>{
-        const user = await this.model.findOneAndUpdate({username}, {password}).exec();
-
-        if(!user) {
-            throw new Error('user not created')
-        }
+    async UpdatePassword(username:Username, password: Password): Promise<MongoDoc<IUser> | null>{
+        const user = await this.model.findOneAndUpdate({username}, {password})
+        .exec().catch((err) => handleDBError());;
 
         return user
     }
