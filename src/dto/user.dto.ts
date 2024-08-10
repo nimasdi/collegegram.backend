e5b2b60a76dbd355a918e18eae9679271aea133c
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-10 22:45:00 +0330

    signup errors handeling required fields

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,11 +1,30 @@
 import { z } from "zod";
 import { zodEmail, zodPassword, zodUsername } from "../types/user.types";
 
+const checkRequired = (data: any,fieldName:string) => {
+    if (data[fieldName] === "undefined") {
+        return false;
+    }
+    return true;
+}
+
 export const createUserDto = z.object({
     username : zodUsername,
     password : zodPassword,
     email: zodEmail
-});
+})
+.refine(data => checkRequired(data,"email"), {
+    message: "email is required",
+    path: ['email'],
+  })
+.refine(data => checkRequired(data,"password"), {
+    message: "password is required",
+    path: ['password'],
+  })
+.refine(data => checkRequired(data,"username"), {
+    message: "username is required",
+    path: ['username'],
+  });
 
 
 export type CreateUserDto = z.infer<typeof createUserDto>;
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -5,20 +5,20 @@ import { Brand } from './Brand';
 // type for firstname and lastname
 export type Name = Brand<string, 'Name'>;
 export const isName = (x: string): x is Name =>
-    x.length > 3 && x.length < 50 
+    x.length >= 3 && x.length <= 50 
 export const zodName = z.coerce.string().refine(isName, {message:"name length should be btween 3 and 50 char"});
 
 // type for username
 export type Username = Brand<string, 'Username'>;
 export const isUsername = (x: string): x is Username =>
-    x.length > 8 && x.length < 50 
+    x.length >= 8 && x.length <= 50 
 export const zodUsername = z.coerce.string().refine(isUsername, {message:"username length should be btween 8 and 50 char"});
 
 
 // type for password
 export type Password = Brand<string, 'Password'>;
 export const isPassword = (x: string): x is Password =>
-    x.length > 8 && x.length < 50 
+    x.length >= 8 && x.length <= 50 
 export const zodPassword = z.coerce.string().refine(isPassword, {message:"password length should be btween 8 and 50 char"});
 
 // type for email
