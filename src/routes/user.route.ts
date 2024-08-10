c4d70d4f843a883e5d8ce893fcb611dde8caa2e9
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-10 21:58:29 +0330

    signup errors handeling

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,6 +4,7 @@ import { createUser } from "../repositrory/user/user.repositroy";
 import { createUserDto } from "../dto/user.dto";
 import { Username, isEmail, isUsername } from '../types/user.types';
 import authMiddleware from '../utility/authorization';
+import { ZodError } from 'zod';
 
 export const UserRoute = (userService: UserService) => {
     const router = Router();
@@ -49,7 +50,11 @@ export const UserRoute = (userService: UserService) => {
                 res.status(200).json({"message" : "ثبت نام با موفقیت انجام شد."})
             }
         } catch (error) {
-            res.status(400).json({"message":"bad request!"})
+            if(error instanceof ZodError){
+                const errorsMessage = error.errors.reduce((prev,e) => {return {...prev,[e.path[0]]:e.message}},{})
+                return res.status(400).json(errorsMessage)
+            }
+            return res.status(500).json({message:"server error"})
         }
     })
 
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -6,20 +6,20 @@ import { Brand } from './Brand';
 export type Name = Brand<string, 'Name'>;
 export const isName = (x: string): x is Name =>
     x.length > 3 && x.length < 50 
-export const zodName = z.coerce.string().refine(isName);
+export const zodName = z.coerce.string().refine(isName, {message:"name length should be btween 3 and 50 char"});
 
 // type for username
 export type Username = Brand<string, 'Username'>;
 export const isUsername = (x: string): x is Username =>
     x.length > 8 && x.length < 50 
-export const zodUsername = z.coerce.string().refine(isUsername);
+export const zodUsername = z.coerce.string().refine(isUsername, {message:"username length should be btween 8 and 50 char"});
 
 
 // type for password
 export type Password = Brand<string, 'Password'>;
 export const isPassword = (x: string): x is Password =>
     x.length > 8 && x.length < 50 
-export const zodPassword = z.coerce.string().refine(isPassword);
+export const zodPassword = z.coerce.string().refine(isPassword, {message:"password length should be btween 8 and 50 char"});
 
 // type for email
 export type Email = Brand<string, 'Email'>;
@@ -29,5 +29,5 @@ export const isEmail = (x: string): x is Email =>
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     )
-export const zodEmail = z.coerce.string().refine(isEmail);
+export const zodEmail = z.coerce.string().refine(isEmail, {message:"email format is not correct"});
 
