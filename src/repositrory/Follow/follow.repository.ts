b090d82d6f4e1110841d89664647d8e1a26deb34
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-19 00:25:51 +0330

    add follow and unfollow in repo

--- src/repositrory/Follow/follow.repository.ts
+++ src/repositrory/Follow/follow.repository.ts
@@ -22,11 +22,21 @@ export class FollowRepository {
         console.log(error)
         throw new HttpError(500,'خطای شبکه رخ داده است.')
     }
+
+    async follow(followerUsername: Username, followingUsername: Username): Promise<void> {
+        const user = new this.model({followerUsername, followingUsername});
+        await user.save().catch((err) => this.handleDBError(err));
+    }
+
+    async unfollow(followerUsername: Username, followingUsername: Username): Promise<void> {
+        await this.model.deleteOne({ followingUsername , followerUsername })
+        .catch((err) => this.handleDBError(err));
+    }
  
     async checkFollow(followerUsername: Username, followingUsername: Username): Promise<Boolean> {
         const followExist = await this.model.findOne({ followingUsername , followerUsername })
         .catch((err) => this.handleDBError(err))
-        
+
         if (!followExist) {
             return false
         }
