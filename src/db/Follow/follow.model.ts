a538c6ea1c2741082bafcdd269d5d35398518bdd
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-20 19:17:01 +0330

    use pre instead of index on follow system

--- src/db/Follow/follow.model.ts
+++ src/db/Follow/follow.model.ts
@@ -1,5 +1,6 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 import { Username } from '../../types/user.types';
+import { HttpError } from '../../utility/error-handler';
 
 // Interface for User attributes
 export interface IFollow extends Document {
@@ -23,7 +24,22 @@ const FollowSchema: Schema<IFollow> = new Schema({
   timestamps: true,
 });
 
-FollowSchema.index({ followerUserName: 1, followingUserName: 1 }, { unique: true });
+FollowSchema.pre('save', async function (next) {
+    const follow = this as IFollow;
+    // Check if a follow relationship with the same follower and following user names already exists
+    const existingFollow = await mongoose.models.Follow.findOne({
+        followerUserName: follow.followerUsername,
+        followingUserName: follow.followingUsername,
+    }).catch((err) => {throw new HttpError(500,"server error.")});
+
+    if (existingFollow) {
+        // If the relationship already exists, throw an error
+        throw new HttpError(400,'This follow relationship already exists.');
+    }
+
+    // If no such relationship exists, proceed with the save operation
+    next();
+});
 
 // Follow Model
 export const Follow: Model<IFollow> = mongoose.model<IFollow>('Follow', FollowSchema);
