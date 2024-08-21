b7eb1d4dbe72d8df2bbdb8c576dfa4e982880b37
nimasdi <ni.saeidi83@gmail.com>
2024-08-21 19:42:47 +0330

    created the model

---
+++ src/db/post/bookmarkPost.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Model,Document , Schema, Types } from "mongoose";
+import { Username } from "../../types/user.types";
+
+export interface ISavePost extends Document {
+    username: Username;
+    postId: Types.ObjectId;
+}
+
+export const savePostSchema: Schema<ISavePost> = new Schema({
+    username: {
+        type: String,
+        ref: 'User',
+        required: true,
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+
+export const LikePost: Model<ISavePost> = mongoose.model<ISavePost>('LikePost', savePostSchema);
