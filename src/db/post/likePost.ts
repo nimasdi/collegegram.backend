ac292dc367fbaf4565df7eb94ddbf5f9e7406cba
nimasdi <ni.saeidi83@gmail.com>
2024-08-21 17:07:07 +0330

    created the like model

---
+++ src/db/post/likePost.ts
@@ -0,0 +1,26 @@
+import mongoose, { Model,Document , Schema, Types } from "mongoose";
+import { Username } from "../../types/user.types";
+
+export interface ILikePost extends Document {
+    username: Username;
+    postId: Types.ObjectId;
+}
+
+
+export const likePostSchema: Schema<ILikePost> = new Schema({
+    username: {
+        type: String,
+        ref: 'User',
+        required: true,
+    },
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+
+export const LikePost: Model<ILikePost> = mongoose.model<ILikePost>('LikePost', likePostSchema);
