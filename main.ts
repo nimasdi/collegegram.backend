ec21e1a76cb6a5e0dffc736d81b8a71bb3266e23
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-22 11:06:02 +0330

    url

--- main.ts
+++ main.ts
@@ -36,7 +36,7 @@ const commentService = new CommentService(userRepo,postRepo, commentRepo,likeCom
 const followService = new FollowService(followRepo,userRepo)
 
 
-const uri = process.env.MONGO_URIME || '';
+const uri = process.env.MONGO_URI || '';
 
 
 const dbConnection = new MongoDBConnection(uri);
