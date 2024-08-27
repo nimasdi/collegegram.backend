371fc48c7af8f948196138f142e2cdd04eb363b6
baharSaeedi <b.saeedi.ha@gmail.com>
2024-08-27 16:06:03 +0330

    add block route and dto

---
+++ src/routes/block.route.ts
@@ -0,0 +1,134 @@
+import { Router } from "express";
+import authMiddleware from "../utility/authorization";
+import { Username, isUsername } from "../types/user.types";
+import { HttpError } from "../utility/error-handler";
+import { handelErrorResponse } from "../utility/habdle-errResponse";
+import { BlockService } from "../services/Block.service";
+import { blockDto } from "../dto/block.dto";
+
+export const BlockRoute = (blockService:BlockService) => {
+    const router = Router();
+
+     /**
+    * @swagger
+    * /block/{username}:
+    *   get:
+    *     summary: check block 
+    *     description: check block
+    *     tags:
+    *       - Block
+    *     parameters:
+    *         - in: path
+    *           name: username
+    *           required: true
+    *           description: The ID of the post to be updated.
+    *           schema:
+    *             type: string
+    *     security:
+    *       - bearerAuth: []
+    *     responses:
+    *       200:
+    *         description: User information retrieved successfully
+    *       404:
+    *         description: User not found
+    *       500:
+    *         description: Internal server error
+    */
+    router.get("/:username", authMiddleware, async(req, res) => {
+        try {
+            const blockerUser = req.user.username
+            const blockingUser = req.params.username.trim()
+            if(!isUsername(blockingUser)){
+                throw new HttpError(400, "check user name Field")
+            }
+
+            const blocked = await blockService.checkBlock(blockingUser, blockerUser)
+
+            return res.status(200).json({blocked})
+        } catch (error) {
+            handelErrorResponse(res,error)
+        }
+    });
+
+   /**
+ * @swagger
+ * /block:
+ *   post:
+ *     summary: block a user
+ *     description: block a user
+ *     tags:
+ *       - Block
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - blockingUsername
+ *             properties:
+ *               blockingUsername:
+ *                 type: string
+ *                 example: johndoe
+ *     responses:
+ *       200:
+ *         description: blocked
+ */
+    router.post("", authMiddleware, async (req, res, next) => {
+        try {
+            const blockerUser: Username = req.user.username
+            if(!blockerUser) {
+                throw new HttpError(400, "user not found.") 
+            }
+
+            const blockingUser = blockDto.parse(req.body)
+            await blockService.block(blockingUser.blockingUsername, blockerUser)
+            res.status(200).json({message:"user blockd."})
+        } catch (error) {
+            handelErrorResponse(res, error)
+        }
+    })
+
+
+    /**
+     * @swagger
+     * /block:
+     *   delete:
+     *     summary: unblock a user
+     *     description: unblock a user
+     *     tags:
+     *       - Block
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             required:
+     *               - blockingUsername
+     *             properties:
+     *               blockingUsername:
+     *                 type: string
+     *                 example: johndoe
+     *     responses:
+     *       200:
+     *         description: blocked
+     */
+    router.delete("", authMiddleware, async (req, res, next) => {
+        try {
+            const blockerUser: Username = req.user.username
+            if(!blockerUser) {
+                throw new HttpError(400, "user not found.") 
+            }
+
+            const blockingUser = blockDto.parse(req.body)
+            await blockService.unblock(blockingUser.blockingUsername, blockerUser)
+            res.status(200).json({message:"user unblocked."})
+        } catch (error) {
+            handelErrorResponse(res, error)
+        }
+    })
+
+    return router
+}
+ 
