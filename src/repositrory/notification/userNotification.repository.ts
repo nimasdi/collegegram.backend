856c64721e76ba414b5a9e3bda6dab37dd63cc3e
baharSaeedi <b.saeedi.ha@gmail.com>
2024-09-01 23:41:54 +0330

    add create notif for user repo function

--- src/repositrory/notification/userNotification.repository.ts
+++ src/repositrory/notification/userNotification.repository.ts
@@ -1,6 +1,7 @@
 import { Model, Document, Types } from 'mongoose';
 import { HttpError } from '../../utility/error-handler';
 import { IUserNotification } from '../../db/notification/userNotification.model';
+import { Username } from '../../types/user.types';
 
 
 export class UserNotificationtRepository {
@@ -15,6 +16,9 @@ export class UserNotificationtRepository {
         throw new HttpError(500, 'خطای شبکه رخ داده است.')
     }
 
-
+    async createNotificationForUser(username:Username, notificationId: Types.ObjectId) : Promise<void> {
+        const notif = new this.model({username, notificationId, seen: false});
+        await notif.save().catch((err) => this.handleDBError(err));
+    }
 
 }
