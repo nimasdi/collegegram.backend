45a5d9396ba45e4bab90a50cff876ecde92d91cb
nimasdi <ni.saeidi83@gmail.com>
2024-09-01 10:01:42 +0330

    created the model

---
+++ src/db/notification/notification.model.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+import { Username } from '../../types/user.types';
+
+enum ActionType {
+    LIKE = "like",
+    COMMENT = "comment",
+    FOLLOW = "follow",
+}
+
+enum TargetEntityType {
+    POST = "postId",
+    COMMENT = "commentId",
+    USER = "userId",
+}
+
+interface INotification {
+    actionCreator: Username;              // User who performed the action
+    actionType: ActionType;               // Type of action performed
+    targetEntityType: TargetEntityType;   // The type of entity that was acted upon
+    targetEntityId: mongoose.Types.ObjectId; // Use ObjectId type for MongoDB references
+    targetUser: Username;                 // User who is the target of the action
+}
+
+const NotificationSchema: Schema<INotification> = new Schema(
+    {
+        actionCreator: {
+            type: String,
+            ref: 'User', 
+            required: true,
+        },
+        actionType: {
+            type: String,
+            enum: Object.values(ActionType),
+            required: true,
+        },
+        targetEntityType: {
+            type: String,
+            enum: Object.values(TargetEntityType),
+            required: true,
+        },
+        targetEntityId: {
+            type: Schema.Types.ObjectId,  
+            required: true,
+            refPath: 'targetEntityType',
+        },
+        targetUser: {
+            type: String,
+            ref: 'User',  
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Notification: Model<INotification> = mongoose.model<INotification>('Notification', NotificationSchema);
+
